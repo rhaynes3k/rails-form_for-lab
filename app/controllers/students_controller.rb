0c47f2a9ae9a0f1cd61ba932f9ee95ca45ef9c5f
rhaynes3k <rhaynes3k@gmail.com>
2021-03-30 21:39:17 -0500

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,35 @@
+class StudentsController < ApplicationController
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to student_path(@student)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+    redirect_to student_path(@student)
+  end
+
+private
+  def student_params
+    params.required(:student).permit(:first_name, :last_name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  # get 'students/create'
+  #
+  # get 'students/new'
+  #
+  # get 'students/show'
+  #
+  # get 'students/edit'
+  #
+  # get 'students/update'
+  #
+  # get 'school_classes/create'
+  #
+  # get 'school_classes/show'
+  #
+  # get 'school_classes/edit'
+  #
+  # get 'school_classes/update'
+  #
+  # get 'school_classes/new'
+resources :school_classes
+resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
